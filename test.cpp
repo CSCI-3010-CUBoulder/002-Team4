2b1bc845d0ced54b4ee2ad0d7d8fe4bcdd8b3a66
Andrew <Andrew.Gelfman@colorado.edu>
2020-02-11 12:03:46 -0700

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,12 +1,16 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 
-// includes as you need them
-
-
 #include "catch.hpp"
 #include "UtilityFunctions.h"
 
 // Your tests go here
 // Each TEST_CASE should test one function
+
+TEST_CASE( "Split function works", "[Split]" ) {
+    std::vector<std::string> t1 = Split("Test Test 123!");
+    std::vector<std::string> a1 = {"Test", "Test", "123!"};
+
+    REQUIRE( t1 == a1 );
+}
 
 // Each SECTION should test one aspect of that functino
