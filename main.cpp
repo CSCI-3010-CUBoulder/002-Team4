c750ddf7e7c7ccdfc4d0bc6b414d686f16b5273c
Andrew <Andrew.Gelfman@colorado.edu>
2020-02-11 11:55:11 -0700

    Setup CI

--- main.cpp
+++ main.cpp
@@ -2,9 +2,5 @@
 #include "UtilityFunctions.h"
 
 int main(int argc, char const *argv[]) {
-    std::vector<std::string> toPrint = Split("Test Test 123!!!", " ");
-    for (size_t i = 0; i < toPrint.size(); i++) {
-        std::cout << toPrint[i] << '\n';
-    }
     return 0;
 }
