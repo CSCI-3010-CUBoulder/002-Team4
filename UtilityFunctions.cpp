b341de4601eb30f7936e4b6ccb98530bcadc7111
Andrew <Andrew.Gelfman@colorado.edu>
2020-02-11 12:13:54 -0700

    Update UtilityFunctions.cpp

--- UtilityFunctions.cpp
+++ UtilityFunctions.cpp
@@ -4,9 +4,11 @@ std::vector<std::string> Split(std::string whole, std::string separator) {
     std::vector<std::string> toReturn;
     int prevLoc = 0;
     for (size_t i = 0; i < whole.size(); i++) {
-        if (whole.substr(i, 1) == separator || i == whole.size()) {
+        if (whole.substr(i, 1) == separator) {
             toReturn.push_back(whole.substr(prevLoc, i-prevLoc));
             prevLoc = i+1;
+        } else if (i == whole.size()-1) {
+            toReturn.push_back(whole.substr(prevLoc, whole.size()-i))
         }
     }
     return toReturn;
