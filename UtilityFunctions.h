f40f86d8f46d68868bc3b5c1cfaeeb61e40f67a7
Andrew <Andrew.Gelfman@colorado.edu>
2020-02-12 23:07:00 -0700

    Typo in UtilityFunctions.h

--- UtilityFunctions.h
+++ UtilityFunctions.h
@@ -1,6 +1,7 @@
 #include <vector>
 #include <string>
+
 std::vector<std::string> Split(std::string, std::string);
 
 // Sums all number in avector and returnsthe resulting value:
-int sum(std::vector<int> nums);
+int Sum(std::vector<int> nums);
